81f6a07388687c72ff5ce5d3234fa3314b1cca67
aarshikjapsen <134580128+aarshikjapsen@users.noreply.github.com>
2023-05-25 11:25:53 +0530

    Create command shell execution.java

---
+++ command shell execution.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CommandInjectionExample {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter a name: ");
+        String name = reader.readLine();
+
+        // Vulnerable code: directly concatenating user input into a shell command
+        String command = "echo Hello, " + name;
+        Process process = Runtime.getRuntime().exec(command);
+
+        // Read the output of the command
+        BufferedReader outputReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String output;
+        while ((output = outputReader.readLine()) != null) {
+            System.out.println(output);
+        }
+        outputReader.close();
+    }
+}
